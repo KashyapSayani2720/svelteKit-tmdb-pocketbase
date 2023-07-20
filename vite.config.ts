554127b095ee223cef590943bad0b0fc3d0d1b2f
Kashyap Sayani <kashyapsayani1@gmail.com>
2023-07-20 20:39:05 +0530

    Added config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite';
 import Unocss from "unocss/vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), Unocss()]
+	plugins: [sveltekit(), Unocss()],
+	server:{
+		watch:{
+			usePolling: true
+		},
+		host: true
+	}
 });
